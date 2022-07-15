bad81e422096d5ae4efeddc71016f4e2a6e5aaa0
Alexey <alexey25ya@mail.ru>
2022-07-15 19:57:09 +0300

    Задача52

---
+++ Задача52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void averageColumn(int[,] array)
+{
+    double aver = 0;
+    double sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine(sum);
+        aver = sum / array.GetLength(0);
+        sum = 0;
+        Console.WriteLine("Среднее арифметическое столбца " + (j + 1) + " равно: " + aver);
+    }
+
+}
+int[,] matrix = new int[5, 6];
+
+FillArray(matrix);
+PrintArray(matrix);
+averageColumn(matrix);
